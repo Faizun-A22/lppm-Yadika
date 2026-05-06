b0ee3bbc03bd9906903b8dc8701c6e3d002ec31f
Faizun-A22 <faizacket@gmail.com>
2026-05-06 17:28:33 +0700

    nams

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.use(helmet({
     crossOriginResourcePolicy: { policy: "cross-origin" }
 }));
 app.use(cors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500'],
+    origin: true,  
     credentials: true
 }));
 app.use(express.json());
